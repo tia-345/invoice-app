36254d5d93a81d309e3d79e461d10a25dfd923b9
Tia Moncy <tiamoncy345@gmail.com>
2026-03-08 14:02:19 +0530

    update111

--- src/History.js
+++ src/History.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./App.css";
 
 function History() {
